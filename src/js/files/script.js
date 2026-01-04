c58f0d54eccc583f3727f3893521734a89a89da7
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2026-01-04 23:02:07 +0300

    Initial commit

---
+++ src/js/files/script.js
@@ -0,0 +1,25 @@
+// Подключение функционала "Чертоги Фрилансера"
+import {
+  isMobile
+} from "./functions.js";
+// Подключение списка активных модулей
+import {
+  flsModules
+} from "./modules.js";
+window.addEventListener("load", function () {
+  document.body.classList.remove("load");
+});
+
+
+// Проверяем наличие элемента .header
+
+const header = document.querySelector('.header');
+if (header) {
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 40) {
+      header.classList.add('_fixed');
+    } else {
+      header.classList.remove('_fixed');
+    }
+  });
+}
