bc756fb7f00cd14f03880e978c646b0c70a59bcc
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2026-01-17 17:47:37 +0300

    Update script.js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -411,7 +411,7 @@ document.addEventListener('DOMContentLoaded', function () {
           // --- Условие: использовать scale или нет ---
           if (this.useScale) {
             // Анимация с масштабированием
-            var scaling = i == this.items.length - 1 ? 1 : (this.cardHeight - scrolling * 0.15) / this.cardHeight;
+            var scaling = i == this.items.length - 1 ? 1 : (this.cardHeight - scrolling * 0.08) / this.cardHeight;
             this.items[i].style.transform = 'translateY(' + this.marginY * i + 'px) scale(' + scaling + ')';
           } else {
             // Анимация без масштабирования (только translateY)
